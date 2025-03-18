e06aa3c9a17554ddd8b89b1e2edcae760453adde
dylan <dylan.tancz@gmail.com>
2025-03-19 00:29:01 +0800

    google maps + tailwind

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import OpenLayersMap from 'vue3-openlayers'
-import 'vue3-openlayers/styles.css' // Required for OpenLayers styles
-import './assets/main.css' // Your Tailwind/global styles
+// import { Map, Layers, Sources, Geometries, Styles } from 'vue3-openlayers'
+// import 'vue3-openlayers/styles.css' // Required for OpenLayers styles
+import './assets/styles/main.css' // Your Tailwind/global styles
 
 const app = createApp(App)
 
-app.use(OpenLayersMap)
+// app.use(Map)
+// app.use(Layers)
+// app.use(Sources)
+// app.use(Styles)
+// app.use(Geometries)
+
 
 app.mount('#app')
